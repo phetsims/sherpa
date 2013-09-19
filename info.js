e726554d5ad9d534c24cfb42f7a88d9d3936cae6
samreid <reids@colorado.edu>
2013-09-19 14:11:51 -0600

    Started info metadata file

---
+++ info.js
@@ -0,0 +1,132 @@
+//License info for all of the projects
+module.exports = function( grunt ) {
+  var licenseInfo = {
+    'almond-0.2.6.js': {
+      text: 'almond 0.2.6 Copyright (c) 2011-2012, The Dojo Foundation All Rights Reserved.\n' +
+            'Available via the MIT or new BSD license.\n' +
+            'see: http://github.com/jrburke/almond for details',
+      usage: ['sim']
+    },
+    'benchmark-1.0.0.js': {
+      text: 'Benchmark.js v1.0.0 <http://benchmarkjs.com/>\n' +
+            'Copyright 2010-2012 Mathias Bynens <http://mths.be/>\n' +
+            'Based on JSLitmus.js, copyright Robert Kieffer <http://broofa.com/>\n' +
+            'Modified by John-David Dalton <http://allyoucanleet.com/>\n' +
+            'Available under MIT license <http://mths.be/mit>',
+      usage: ['development']
+    },
+    'syntaxhighlighter-3.0.83': {
+      text: 'SyntaxHighlighter\n' +
+            'http://alexgorbatchev.com/SyntaxHighlighter\n' +
+            '\n' +
+            'SyntaxHighlighter is donationware. If you are using it, please donate.\n' +
+            'http://alexgorbatchev.com/SyntaxHighlighter/donate.html\n' +
+            '\n' +
+            '@version\n' +
+            '3.0.83 (July 02 2010)\n' +
+            '\n' +
+            '@copyright\n' +
+            'Copyright (C) 2004-2010 Alex Gorbatchev.\n' +
+            '\n' +
+            '@license\n' +
+            'Dual licensed under the MIT and GPL licenses.',
+      usage: ['docs']
+    },
+    'bootstrap-2.2.2': {
+      text: 'Bootstrap v2.2.2\n' +
+            '\n' +
+            'Copyright 2012 Twitter, Inc\n' +
+            'Licensed under the Apache License v2.0\n' +
+            'http://www.apache.org/licenses/LICENSE-2.0\n' +
+            '\n' +
+            'Designed and built with all the love in the world @twitter by @mdo and @fat.',
+      usage: ['docs']
+    },
+    'canvg-1.2': {
+      text: '',
+      usage: ['development'],
+      notes: 'used in scenery unit tests'
+    },
+    'easeljs-0.5.0.min.js': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'has.js': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'i18n-2.0.4': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'jquery.mobile-1.3.1.min.css': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'jquery-2.0.3': {
+      text: 'jQuery JavaScript Library v2.0.n\n' +
+            'http://jquery.com/\n' +
+            '\n' +
+            'Includes Sizzle.js\n' +
+            'http://sizzlejs.com/\n' +
+            '\n' +
+            'Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors\nReleased under the MIT license\n' +
+            'http://jquery.org/license\n' +
+            '\n' +
+            'Date: 2013-07-03T13:30Z',
+      usage: ['sim']
+    },
+    'jshint-2.1.2': {
+      text: '',
+      usage: [],
+      notes: ''},
+    'lodash-2.0.0': {
+      text: '',
+      usage: [],
+      notes: ''},
+    'numeric-1.2.6': {
+      text: '',
+      usage: [],
+      notes: ''},
+    'PxLoader': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'qhint': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'qunit-1.12.0': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'require-2.1.8': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'rgbcolor': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'seedrandom-2.2': {
+      text: '',
+      usage: [],
+      notes: ''
+    },
+    'stats.min.js': {
+      text: '',
+      usage: [],
+      notes: ''
+    }
+  };
+  return licenseInfo;
+};
