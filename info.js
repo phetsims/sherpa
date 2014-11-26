40891ef0a2822865833ee5450e1460edb7881651
Sam Reid <reids@colorado.edu>
2014-11-26 09:53:52 -0700

    Fixed filename + license info for box2d

--- info.js
+++ info.js
@@ -58,14 +58,15 @@ module.exports = function( grunt ) {
       usage: ['docs'],
       notes: ''
     },
-    'Box2D': {
-      text: 'Box2D r29\n' + //use the subversion version because it doesn't seem to be in any release cycle
+    'box2d.min': {
+      text: 'Box2D r29\n' +
             '\n' +
             'Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com\n' +
             'Licensed under the zlib License\n' +
-            'https://box2dweb.googlecode.com/svn/trunk/Box2D.js'
-      usage: ['sim'], // But only for Sugar and Salt Solutions as of Nov 26, 2014
-      notes: ''
+            'https://box2dweb.googlecode.com/svn/trunk/Box2D.js',
+      usage: ['sim'],
+      notes: 'Only for Sugar and Salt Solutions as of Nov 26, 2014' +
+             'Use the subversion version because it doesnt seem to be in any release cycle'
     },
     'canvg-1.3': {
       text: 'canvg.js - Javascript SVG parser and renderer on Canvas\n' +
