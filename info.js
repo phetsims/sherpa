5c5f9859074811d398cc0e85b60369449e7879b4
Sam Reid <reids@colorado.edu>
2014-12-01 09:29:53 -0700

    Updated to latest version of text plugin, see https://github.com/phetsims/scenery/issues/322

--- info.js
+++ info.js
@@ -189,8 +189,8 @@ module.exports = function( grunt ) {
       usage: ['development'],
       notes: ''
     },
-    'text': {
-      text: 'RequireJS text 2.0.5 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.\n' +
+    'text-2.0.12': {
+      text: 'RequireJS text 2.0.12 Copyright (c) 2010-2014, The Dojo Foundation All Rights Reserved.\n' +
             'Available via the MIT or new BSD license.\n' +
             'http://github.com/requirejs/text for details',
       usage: ['sim'],
