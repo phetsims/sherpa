231610ebc8c416671c3ab25c9ce5f27b011fb6b4
samreid <reids@colorado.edu>
2016-07-14 14:20:38 -0600

    Fixed lint issues, see https://github.com/phetsims/sherpa/issues/64

--- js/checkout-published-sims.js
+++ js/checkout-published-sims.js
@@ -9,7 +9,7 @@
   // http://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
   var download = function( url, dest, cb ) {
     var file = fs.createWriteStream( dest );
-    var request = http.get( url, function( response ) {
+    http.get( url, function( response ) {
       response.pipe( file );
       file.on( 'finish', function() {
         file.close( cb );  // close() is async, call cb after close completes.
