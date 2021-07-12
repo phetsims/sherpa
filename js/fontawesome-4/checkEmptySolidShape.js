4d9ec58a24e8c385a0b49113d377d4d727d7c1cf
samreid <reids@colorado.edu>
2021-07-12 16:03:41 -0600

    Use Shape/Path instead of FontAwesomeNode for Checkbox, see https://github.com/phetsims/sherpa/issues/83

---
+++ js/fontawesome-4/checkEmptySolidShape.js
@@ -0,0 +1,11 @@
+/**
+ Copyright (c) 2013, Dave Gandy - http://fontawesome.io
+ with Reserved Font Name Font Awesome.
+
+ This Font Software is licensed under the SIL Open Font License, Version 1.1.
+ This license is copied below, and is also available with a FAQ at:
+ http://scripts.sil.org/OFL
+ */
+import Shape from '../../../kite/js/Shape.js';
+
+export default new Shape( 'M1120 1280h-832q-66 0 -113 -47t-47 -113v-832q0 -66 47 -113t113 -47h832q66 0 113 47t47 113v832q0 66 -47 113t-113 47zM1408 1120v-832q0 -119 -84.5 -203.5t-203.5 -84.5h-832q-119 0 -203.5 84.5t-84.5 203.5v832q0 119 84.5 203.5t203.5 84.5h832 q119 0 203.5 -84.5t84.5 -203.5z' );
