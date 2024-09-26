8a7a7b66b92c3e29c1ff2f4f149e421b13298cbd
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:36 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for sherpa.
+ * ESLint configuration for sherpa.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
