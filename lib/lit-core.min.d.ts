c759e483f3d2c3fa02cd36f0f475e9fc5740f6da
Sam Reid <reids@colorado.edu>
2026-02-19 13:50:05 -0700

    Add accessibleTemplate, see https://github.com/phetsims/scenery/issues/1776

---
+++ lib/lit-core.min.d.ts
@@ -0,0 +1,17 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * Type declarations for lit-html (lit-core.min.js).
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+type TemplateResult = {
+  readonly _$litType$: number;
+  readonly strings: TemplateStringsArray;
+  readonly values: readonly unknown[];
+};
+export function html( strings: TemplateStringsArray, ...values: unknown[] ): TemplateResult;
+export function render( value: TemplateResult, container: HTMLElement | DocumentFragment ): void;
+export const nothing: symbol;
+export type { TemplateResult };
